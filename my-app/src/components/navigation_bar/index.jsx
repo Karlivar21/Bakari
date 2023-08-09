a3f975fb8d4af978a672e64ec6248f202c2e7bca
Karlivar21 <karlk21@ru.is>
2023-08-09 18:50:43 +0000

    comment_out_carousel

--- my-app/src/components/navigation_bar/index.jsx
+++ my-app/src/components/navigation_bar/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles.css';
 import { NavigationLink } from '../navigation_link/index.jsx';
 import coverlogo from '../../resources/coverlogo.png'
-import carret from '../../resources/carret.svg.png'
 
 export const NavigationBar = () => {
 
@@ -14,7 +13,7 @@ export const NavigationBar = () => {
       <ul className="navigation-links">
         <NavigationLink title="Forsíða" href="/"/>
         <div className="navigation-menu">
-          <button className="navigation-menu__button"> Vörur <img className='carret' src={carret}/></button>
+          <button className="navigation-menu__button"> Vörur <i className="fa fa-caret-down" aria-hidden="true"/></button>
           <ul className="navigation-menu__content">
             <NavigationLink title="Brauð" href="/braud"/>
             <NavigationLink title="Rúnstykki" href="/runstykki"/>
